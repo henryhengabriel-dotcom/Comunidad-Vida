953633889d5f2167bee3a9255615842f8ec1ece8
Henry Perdomo <luahdev@gmail.com>
2026-02-26 19:36:04 -0300

    refactorizacion de claves

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
 from flask import Flask
 from utils.db import db
 from routes.contacto import contacto_bp
-from dotenv import load_dotenv
-import os
+from config.config import Config
 
 
 
 app = Flask(__name__)
-load_dotenv()
-
-
-DB_USER= os.getenv('DB_USER')
-DB_PASSWORD=os.getenv('DB_PASSWORD')
-DB_HOST=os.getenv('DB_HOST')
-DB_PORT=os.getenv('DB_PORT')
-DB_NAME=os.getenv('DB_NAME')
-SECRET_KEY=os.getenv('SECRET_KEY')
-FLASK_ENV=os.getenv('FLASK_ENV')
-
-
-
-
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config.from_object(Config)
 
 app.register_blueprint(contacto_bp) 
 db.init_app(app)
